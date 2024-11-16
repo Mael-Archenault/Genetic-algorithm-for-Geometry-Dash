d5beb80876b545028889759d2a375ee034b90d0e
Mael-Archenault <mael.archenault@gmail.com>
2024-11-16 01:04:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ pygame.init()
 screen = pygame.display.set_mode((1080,720))
 
 
+# USER-CODE-END
+
 # trainer = Trainer(screen, "stereo_madness", "./save",None, 1000)
 # trainer.run()
 
 # tester = Tester(screen, "stereo_madness" , None)
 # tester.run()
 
-level_editor = LevelEditor(screen, "stereo_madness")
-level_editor.run()
+#level_editor = LevelEditor(screen, "stereo_madness")
+#level_editor.run()
 
+
+# USER-CODE-END
 pygame.quit()
